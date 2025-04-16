ec178589ff9bbd86916b957d0710fc3a066e5b38
Gamabyta24 <Gamabyta@gmail.com>
2025-04-16 16:17:29 +0300

    Обновленно: маршрутизация

--- django_blog/users/urls.py
+++ django_blog/users/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from .views import index
 
-from .views import UserCreateView,UserLoginView
+from .views import UserCreateView,UserLoginView,UserLogoutView
 
 urlpatterns = [
     path("", index, name="index"),
     path("register/", UserCreateView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
 ]
