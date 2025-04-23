7f5f8a94656d8509de62b01266cfa12b7a59fe72
Gamabyta24 <Gamabyta@gmail.com>
2025-04-23 06:16:03 +0300

    Добавлено: модель категорий

--- django_blog/categories/models.py
+++ django_blog/categories/models.py
@@ -1,3 +1,20 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="URL")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category_posts", kwargs={"slug": self.slug})
