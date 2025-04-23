610146424f1d42037a586eff2891e1bca30d0632
Gamabyta24 <Gamabyta@gmail.com>
2025-04-23 08:06:41 +0300

    Добавлено: форма заполнения для Category , пути для Category

---
+++ django_blog/categories/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+import django_blog.categories.views as views
+from django_blog.views import UniversalDeleteView
+urlpatterns = [
+    path('', views.CategoryListView.as_view(), name='category_list'),
+    path('category/<slug:slug>/', views.CategoryPostListView.as_view(), name='category_posts'),
+    path('new/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('category/<slug:slug>/edit/', views.CategoryUpdateView.as_view(), name='category_edit'),
+    path('<slug:slug>/delete/', UniversalDeleteView.as_view(), {'model_name': 'category'} ,name='category_delete'),
+    ]
