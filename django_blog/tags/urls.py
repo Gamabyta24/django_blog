f7cede82b1b8653015edd8aa8167e9d71410ed69
Gamabyta24 <Gamabyta@gmail.com>
2025-04-25 00:02:44 +0300

    Поправлены ссылки

--- django_blog/tags/urls.py
+++ django_blog/tags/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', TagListView.as_view(), name='tag_list'),
     path('new/', TagCreateView.as_view(), name='tag_create'),
     path('<slug:slug>/', TagPostListView.as_view(), name='tag_posts'),
-    path('tag/<slug:slug>/edit/', TagUpdateView.as_view(), name='tag_edit'),
+    path('<slug:slug>/edit/', TagUpdateView.as_view(), name='tag_edit'),
     path('<slug:slug>/delete/', UniversalDeleteView.as_view(), {'model_name': 'tag'},name='tag_delete'),
     ]
