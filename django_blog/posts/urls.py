49e90a50da5ea314f33117170fd4f8c4358c5eef
Gamabyta24 <Gamabyta@gmail.com>
2025-04-24 12:01:53 +0300

    Исправлены названия путей

--- django_blog/posts/urls.py
+++ django_blog/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import PostDetailView , PostCreateView , PostUpdateView , PostDeleteView
+from .views import PostCreateView , PostUpdateView ,  PostListView
 from django_blog.views import UniversalDeleteView
 urlpatterns = [
+    path('', PostListView.as_view(), name='home'),
     path('new/', PostCreateView.as_view(), name='post_create'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/<slug:slug>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('<slug:slug>/', PostListView.as_view(), name='post_detail'),
+    path('<slug:slug>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('<slug:slug>/delete/', UniversalDeleteView.as_view(),{'model_name': 'post'}, name='post_delete'),
     ]
