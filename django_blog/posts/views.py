655e3fbcc21a10420a9d210dded9c3ad921c3fb8
Gamabyta24 <Gamabyta@gmail.com>
2025-04-24 12:03:40 +0300

    Добавлено: представление фильтрации

--- django_blog/posts/views.py
+++ django_blog/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView , ListView
 from .models import Post
 from django_blog.categories.models import Category
 from django_blog.tags.models import Tag
@@ -12,14 +12,44 @@ def index(request):
     return render(request, 'posts/index.html')
 
 
-class PostDetailView(DetailView):
+class PostListView(ListView):  # Было: PostDetailView(DetailView)
     model = Post
-    template_name = 'posts/post_detail.html'
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = Post.objects.all().order_by('-created_date')
+
+        # Фильтрация по категориям
+        categories = self.request.GET.getlist('categories')
+        if categories:
+            queryset = queryset.filter(category__id__in=categories)
+
+        # Фильтрация по тегам
+        tags = self.request.GET.getlist('tags')
+        if tags:
+            queryset = queryset.filter(tags__id__in=tags).distinct()
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['tags'] = Tag.objects.all()
+        context['selected_categories'] = self.request.GET.getlist('categories')
+        context['selected_tags'] = self.request.GET.getlist('tags')
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['tags'] = Tag.objects.all()
+
+        # Эти переменные нужны для отображения checked-состояний
+        context['selected_categories'] = self.request.GET.getlist('categories')
+        context['selected_tags'] = self.request.GET.getlist('tags')
+
         return context
 
 
@@ -42,7 +72,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'posts/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
